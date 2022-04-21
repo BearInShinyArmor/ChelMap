767e76a30c3116480673c8434307c6ac009262bb
BearInShinyArmor <bearinarmor@gmail.com>
2022-04-21 15:17:03 +0500

    Full project

---
+++ Assets/IconController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class IconController : MonoBehaviour
+{
+    
+    public MainController MainController;
+  
+    public bool isActive = false;
+    public float enlargedScale = 2;
+    public float enlargedHeith = 4;
+    public float enlargeSpeed = 4;
+    public float initialX;
+    public float initialY;
+    private UnityAction someListener;
+    private bool lastActive;
+
+    void Awake()
+    {
+        someListener = new UnityAction(Reset);
+    }
+    void OnEnable()
+    {
+        EventManager.StartListening("Reset", someListener);
+    }
+    void OnDisable()
+    {
+        EventManager.StopListening("Reset", someListener);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        enlargedScale = MainController.enlargeScale;
+        enlargedHeith = MainController.enlargeHeight;
+        enlargeSpeed = MainController.enlargeSpeed;
+        initialX = transform.position.x;
+        initialY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            if (transform.localScale.x < enlargedScale)
+            {
+                transform.localScale += new Vector3(Time.deltaTime * enlargeSpeed, Time.deltaTime * enlargeSpeed, Time.deltaTime * enlargeSpeed);
+            }
+            else
+            {
+                transform.localScale = new Vector3(enlargedScale, enlargedScale, enlargedScale);
+            }
+
+            if (transform.position.y < enlargedHeith)
+            {
+                transform.position += new Vector3(0, Time.deltaTime * enlargeSpeed, 0);
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, enlargedHeith, transform.position.z);
+            }
+        }
+        else
+        {
+            if (transform.localScale.x >= 1)
+            {
+                transform.localScale -= new Vector3(Time.deltaTime * enlargeSpeed, Time.deltaTime * enlargeSpeed, Time.deltaTime * enlargeSpeed);
+                if (transform.localScale.x < 1)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+            }
+            if (transform.position.y >= initialY)
+            {
+                transform.position -= new Vector3(0, Time.deltaTime * enlargeSpeed, 0);
+                if (transform.position.y < initialY)
+                {
+                    transform.position += new Vector3(0, initialY - transform.position.y, 0);
+                }
+            }
+        }
+    }
+    void OnMouseUp()
+    {
+        Debug.Log(gameObject.name + " get click");
+        MainController.SendCommand(gameObject.name);
+
+
+        if (lastActive)
+        {
+            EventManager.TriggerEvent("Reset");            
+        }
+        else
+        {
+            EventManager.TriggerEvent("Reset");
+            isActive = true;// !isActive;
+        }
+    }
+
+    public void Reset()
+    {
+        lastActive = isActive;
+        //Debug.Log(gameObject.name + " reseved message");
+        isActive = false;
+    }
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Configuration;
+using System.Globalization;
+using System.Text;
+using UnityEngine.Events;
+
+public class MainController : MonoBehaviour
+{
+    public float enlargeSpeed;
+    public float enlargeScale;
+    public float enlargeHeight;
+
+   
+
+    private IPAddress ips;
+    private int port;
+    private byte[] bytes = new byte[1024];
+
+    private Dictionary<int, UdpClient> senders = new Dictionary<int, UdpClient>();
+    private Dictionary<int, IPEndPoint> ipEndPoints = new Dictionary<int, IPEndPoint>();
+
+    #region private
+
+    public void SetupSocket()
+    {
+        var ipString = @"127.0.0.1";
+        port = 10998;
+        ips = IPAddress.Parse(ipString);
+        ipEndPoints.Add(port, new IPEndPoint(ips, port));
+        senders.Add(port, new UdpClient());
+
+    }
+    #endregion
+    public void SendCommand(string command)
+    {
+        port = 10998;
+        byte[] msg = Encoding.UTF8.GetBytes(command);
+        int bytesSent = senders[port].Send(msg, msg.Length, ipEndPoints[port]);
+        Debug.Log(command);
+        Debug.Log(port);
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+        SetupSocket();
+        Debug.Log(port);
+        Debug.Log(ips);
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            EventManager.TriggerEvent("Reset");
+        }
+    }
+}
